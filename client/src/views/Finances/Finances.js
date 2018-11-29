4c8cf99a2954fc9346d839ceb04fe513bc8deb3e
nunocr <nunocr964@gmail.com>
2018-11-29 16:24:30 +0000

    finances core view content added

--- client/src/views/Finances/Finances.js
+++ client/src/views/Finances/Finances.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
-import { Container } from 'reactstrap';
+import { Container, Row, Col, CardColumns, Card, CardBody, CardHeader } from 'reactstrap';
+import { Line } from 'react-chartjs-2';
+import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
 
 import {
     AppBreadcrumb,
@@ -23,14 +25,42 @@ import routes from '../../routes';
 import DefaultHeader from '../../containers/DefaultLayout/DefaultHeader';
 import DefaultFooter from '../../containers/DefaultLayout/DefaultFooter';
 
-//<Redirect from="/" to="/dashboard" />
-//^ botar isto antes do </Switch>
+const line = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'My First dataset',
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [65, 59, 80, 81, 56, 55, 40],
+      },
+    ],
+  };
 
-//Charts stuff
-import { Bar, Doughnut, Line, Pie, Polar, Radar } from 'react-chartjs-2';
-import { Card, CardBody, CardColumns, CardHeader } from 'reactstrap';
-import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
+const options = {
+    tooltips: {
+        enabled: false,
+        custom: CustomTooltips
+    },
+    maintainAspectRatio: false
+}
   
+
 class Finances extends Component {
     render() {
         return (
@@ -50,8 +80,37 @@ class Finances extends Component {
                     <main className="main">
                     <AppBreadcrumb appRoutes={routes} />
                   
-                        <div className="animated fadeIn">
-                        gandas finanças aqui ó
+                        <div className="animated fadeIn">                        
+                            <Row>
+                                <Col>
+                                    <Card className="text-white bg-primary">
+                                        <CardHeader>
+                                            <div className="text-value">Profit and Loss Statement</div>
+                                        </CardHeader>
+                                        <CardBody className="pb-0 bg-info">
+                                            <div className="text-value">Sales and Revenue:</div>
+                                            <div>420 hehe xd</div>
+                                            <div className="text-value">Operating Costs:</div>
+                                            <div>666 haha</div>
+                                        </CardBody>
+                                    </Card>
+                                </Col>
+
+                                <Col>
+                                    <Card>
+                                        <CardHeader>
+                                            <div className="text-value">Finances</div>
+                                            <div className="text-value">Operating Costs, Sales and Revenues</div>
+                                        </CardHeader>
+                                        <CardBody>
+                                            <div className="chart-wrapper">
+                                                <Line data={line} options={options} />
+                                            </div>
+                                        </CardBody>
+                                    </Card>
+                                </Col>
+
+                            </Row>                            
                         </div>
 
                     </main>
